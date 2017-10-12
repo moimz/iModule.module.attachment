e084cba8212ec2a95dd12cd4bb5d9218946fb76b
arzzcom <arzz@arzz.com>
2017-10-12 15:53:45 +0900

    Add admin panel

---
+++ admin/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * 이 파일은 iModule 첨부파일모듈의 일부입니다. (https://www.imodule.kr)
+ *
+ * 첨부파일모듈 관리자 패널을 생성한다.
+ * 
+ * @file /modules/attachment/admin/index.php
+ * @author Arzz (arzz@arzz.com)
+ * @license MIT License
+ * @version 3.0.0.160910
+ */
+if (defined('__IM__') == false) exit;
+?>
+<script>
+Ext.onReady(function () { Ext.getCmp("iModuleAdminPanel").add(
+	
+); });
+</script>
