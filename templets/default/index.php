ef2c4bc28b1d8aeb4cea47ad2654b38e665c39dd
arzzcom <arzz@arzz.com>
2018-09-17 18:50:23 +0900

    첨부파일 템플릿 수정

--- templets/default/index.php
+++ templets/default/index.php
@@ -1,3 +1,16 @@
+<?php
+/**
+ * 이 파일은 iModule 첨부파일모듈의 일부입니다. (https://www.imodule.kr)
+ *
+ * 첨부파일모듈 기본 템플릿
+ * 
+ * @file /modules/attachment/templets/default/index.php
+ * @author Arzz (arzz@arzz.com)
+ * @license MIT License
+ * @version 3.0.0
+ * @modified 2018. 9. 17.
+ */
+?>
 <div class="button">
 	<button type="button" data-action="select"><i class="mi mi-upload"></i><span><?php echo $me->getButtonText(); ?></span></button>
 	
